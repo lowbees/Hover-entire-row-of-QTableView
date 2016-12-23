460472d382aba4a48de39f009d6e570cf2554d53
lowbees <demoresh@126.com>
2016-12-23 14:35:43 +0800

    Add files via upload

---
+++ styleddelegate.h
@@ -0,0 +1,28 @@
+﻿#ifndef STYLEDDELEGATE_H
+#define STYLEDDELEGATE_H
+
+#include <QStyledItemDelegate>
+
+class StyledDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    enum HoverBehavior {
+        HoverItems,
+        HoverRows,
+        HoverColumns
+    };
+
+    void setHoverBehavior(HoverBehavior behavior) {
+        hoverBehavior = behavior;
+    }
+
+public:
+    explicit StyledDelegate(QObject *parent = 0);
+    void paint(QPainter *painter, const QStyleOptionViewItem &option,
+               const QModelIndex &index) const;
+private:
+    HoverBehavior hoverBehavior;
+};
+
+#endif // STYLEDDELEGATE_H
